9c67fd929165554f17a3cb58604604f4a2403410
Artur Seletskiy <artur.2304@mail.ru>
2012-08-03 10:00:27 +0400

    autoload перенес в core

--- trailer.php
+++ trailer.php
@@ -3,21 +3,7 @@ require_once 'settings.php';
 require_once 'trailer/core.php';
 
 
-function __autoload($class){
-	if(!class_exists($class)){
-		
-		$class = strtolower($class);
-		
 
-		if(substr_count($class,'controller') != 0){
-			$class = str_replace('controller','_controller',$class);
-			require_once "controllers/$class.php";
-		}
-		else{
-			require_once "models/$class.php";
-		}
-	}
-}
 
 $Core = new Core();
 
--- trailer/core.php
+++ trailer/core.php
@@ -1,4 +1,21 @@
 <?php
+
+function __autoload($class){
+	if(!class_exists($class)){
+		
+		$class = strtolower($class);
+		
+
+		if(substr_count($class,'controller') != 0){
+			$class = str_replace('controller','_controller',$class);
+			require_once "controllers/$class.php";
+		}
+		else{
+			require_once "models/$class.php";
+		}
+	}
+}
+
 class Core{
 
 	var $PATH  = '';
